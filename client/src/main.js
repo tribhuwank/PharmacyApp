41bf30a9d87a91c86b37876a865099a901c516d0
Tribhuwan Kushwaha <ktribhuwan@outlook.com>
2017-12-30 20:44:45 +0545

    Vuetify is used instead of elementui

--- client/src/main.js
+++ client/src/main.js
@@ -3,16 +3,22 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import Vuetify from 'vuetify'
+import { sync } from 'vuex-router-sync'
+import 'vuetify/dist/vuetify.min.css'
+import store from '@/store/store'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+
+Vue.use(Vuetify)
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
