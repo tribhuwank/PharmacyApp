dc975d92cbf857436f3f03be6e0ee2ceb3bc7266
Tribhuwan Kushwaha <ktribhuwan@outlook.com>
2017-12-30 23:47:04 +0545

    new client with sidebar toggle is implemented with login and signup

--- client/src/components/Sidebar.vue
+++ client/src/components/Sidebar.vue
@@ -1,4 +1,27 @@
 <<template>
+  <v-navigation-drawer persistent :mini-variant="miniVariant" 
+      :clipped="toolabar.clippedLeft && !nav.right || toolabar.clippedRight"
+      v-model="drawer" enable-resize-watcher app :right="nav.right" >
+      <v-list>
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i" >
+          <v-list-tile-action>
+            <v-icon light v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
 
 </template>
-
+<<script>
+export default {
+  data(){
+     return {
+      miniVariant: false
+     }
+      
+  }
+}
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/components/Main'
+import Register from '@/components/Register'
+import Login from '@/components/Login'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
     }
   ]
 })
